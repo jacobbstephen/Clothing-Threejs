50830d9fd9c491c13a52c9686b63f4ffdcdc3395
vidhya-vjkumar <106170027+vidhya-vjkumar@users.noreply.github.com>
2024-07-21 20:02:12 +0530

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,12 +1,15 @@
 import CustomButton from "./CustomButton";
-import AIPicker from "./AIPicker"
-import ColorPicker from "./ColorPicker"
-import FilePicker from "./FilePicker"
-import Tab from "./Tab"
+import ModelPicker from './ModelPicker'
+import ColorPicker from './ColorPicker'
+import FilePicker from './FilePicker'
+import Tab from './Tab'
+
+
 export {
-    AIPicker,
-    ColorPicker,
     CustomButton,
+    ModelPicker,
+    ColorPicker,
     FilePicker,
-    Tab,
-};
+    Tab
+
+}
